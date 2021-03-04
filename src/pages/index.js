5d718332a3f755e7cc8f31467e7d4af4c01c7ce3
Agrobasket <79566472+Agrobasket@users.noreply.github.com>
2021-03-04 13:08:21 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,10 @@ import React, {useEffect} from "react"
 import SEO from "../components/seo"
 
 const IndexPage = () => {
-  window.location = '/loginPage/';
+  useEffect(() => {
+    navigate('/loginPage/');
+  }, []);
+  return null;
 }
 
 export default IndexPage
